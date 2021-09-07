994272f474e2ae1771d84c23416d15907392ea89
daulkim <kimdu0825@gmail.com>
2021-09-08 00:15:24 +0900

    CreateBoard 기능추가

--- src/board/board.service.ts
+++ src/board/board.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
-import { Board } from './board.model';
+import { Board, BoardStatus } from './board.model';
+import {v1 as uuid} from 'uuid';
 
 @Injectable()
 export class BoardService {
@@ -7,7 +8,19 @@ export class BoardService {
     // For test without DB
     private boards: Board[] = [];
 
-    getAllBoards():  Board[]{
+    getAllBoards():  Board[] {
         return this.boards;
+    }
+
+    createBoard(title: string, description: string) {
+        const board = {
+            id: uuid(),
+            title,
+            description,
+            status: BoardStatus.PUBLIC
+        }
+
+        this.boards.push(board);
+        return board;
     }
 }
