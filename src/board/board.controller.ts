36bd4f662e11b7a6957c07401782ac9d0d5a766c
daulkim <kimdu0825@gmail.com>
2021-09-07 22:03:24 +0900

    Board Service 생성

--- src/board/board.controller.ts
+++ src/board/board.controller.ts
@@ -1,7 +1,9 @@
 import { Controller } from '@nestjs/common';
+import { BoardService } from './board.service';
 
 @Controller('board')
 export class BoardController {
 
-    
+    constructor(private boardService: BoardService){}
+
 }
